d83877cd7c23ee4126eacd1334e955ca4e8240e8
Jonathan <ouellettejonathan87@gmail.com>
2026-04-07 18:07:22 +0000

    COMKE ON

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ function draw() {
 }
 
 function keyPressed() {
-  if (window.gm.gameState === 'menu' && (key === ' ' || keyCode === ENTER)) {
+  if (window.gm.gameState === 'menu' && (key === SPACEBAR || keyCode === ENTER)) {
     window.gm.startGame();
   } else if (window.gm.gameState === 'gameover' && (key === 'r' || key === 'R')) {
     window.gm.gameState = 'menu';
