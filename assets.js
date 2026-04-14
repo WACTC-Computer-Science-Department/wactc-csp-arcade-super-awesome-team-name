5cb5fd72a0f569fe032f92f37bd05d902760182c
Jonathan <ouellettejonathan87@gmail.com>
2026-04-14 17:07:55 +0000

    fvzsgszrg

--- assets.js
+++ assets.js
@@ -48,7 +48,7 @@ function loadAssets() {
   schermBalloonImg = loadImage('assets/schermBalloonSprite.png');
   cursorImg = loadImage('assets/cursorSprite.png');
   projectileImg = loadImage('assets/projectileSprite.png');
-  backgroundImg = loadImage('assets/Frontyard-1.png (1).png');
+  backgroundImg = loadImage('assets/Frontyard-1.png (1) - Copy.png');
   gameOverImg = loadImage('assets/gameoversprite.gif');
   menuImg = loadImage('assets/menu image.png');
 }
